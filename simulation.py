0fcc70f5df24930786bf2601710990f72e27c943
Patrik <Patrikkolaric92@gmail.com>
2021-05-21 10:22:25 -0700

    Addind structure for simulation script

---
+++ simulation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#implement simulation here
+
+class Simulation:
+	def __init__(self, system):
+
+	def simulate(self):
+		# while loop that propagates the states of the system in each iteration
+
+class Husky:
+	def __init__(self):
+		#initialize the model of Husky here
+	def propagateStates(self, controlInput = None):
+		#implement one step propagation of system states here
+
+#implement unit tests and call them from main function; make sure to include this python module in the python main.py script
+		
